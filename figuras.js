5ae8a3e4075c1d295cb99c63b8684a7d728dc120
Cinthia Garciaa <ckstgarcia@gmail.com>
2022-03-06 16:59:58 -0700

    estructurando las formulas con funciones

--- figuras.js
+++ figuras.js
@@ -1,56 +1,45 @@
 // código del cuadrado
 console.group("cuadrados");
 
-const LADO_CUADRADO = 5;
-console.log(`Los lados del cuadrado miden: ${LADO_CUADRADO} unidades`);
+function perimetroCuadrado(lado) {
+    return lado * 4;
+}
 
-const PERIMETRO_CUADRADO = LADO_CUADRADO * 4;
-console.log(`El perímetro del cuadrado es: ${PERIMETRO_CUADRADO} unidades`);
+function areaCuadrado(lado) {
+    return lado * lado;
+}
 
-const AREA_CUADRARO = LADO_CUADRADO * LADO_CUADRADO;
-console.log(`El área del cuadrado es: ${AREA_CUADRARO} unidades cuadradas`);
 console.groupEnd();
 
 // código del triángulo
 console.group("triángulos");
 
-const LADO_TRIANGULO1 = 6;
-const LADO_TRIANGULO2 = 6;
-const BASE_TRIANGULO = 4;
-const ALTURA_TRIANGULO = 5.5;
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 +base;
+}
 
-console.log(
-    `Los lados del triángulo miden: 
-    ${LADO_TRIANGULO1} unidades, 
-    ${LADO_TRIANGULO2} unidades, 
-    ${BASE_TRIANGULO} unidades`
-);
+function areaTriangulo(base, altura) {
+    return (base * altura) / 2;
+}
 
-const PERIMETRO_TRIANGULO = 
-LADO_TRIANGULO1 + 
-LADO_TRIANGULO2 + 
-BASE_TRIANGULO;
-
-console.log(`El perímetro del triángulo es: ${PERIMETRO_TRIANGULO} unidades`);
-
-const AREA_TRIANGULO = (BASE_TRIANGULO * ALTURA_TRIANGULO) / 2;
-console.log(`El área del triángulo es: ${AREA_TRIANGULO} unidades cuadradas`);
 console.groupEnd(); 
 
 // código del círculo
 console.group("círculos");
-
-const RADIO_CIRCULO = 4;
 const PI = Math.PI;
 
-console.log(
-    `El radio del círculo es de: ${RADIO_CIRCULO} unidades,
-    el valor de pi es de : ${PI}`
-);
+//Diamétro
+function diametroCirculo(radio) {
+    return radio *2;
+}
 
-const PERIMETRO_CIRCULO = PI * (RADIO_CIRCULO * 2);
-console.log(`El perímetro o la circunferencia del círculo es: ${PERIMETRO_CIRCULO} unidades`);
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return PI * diametro;
+}
 
-const AREA_CIRCULO = PI * (RADIO_CIRCULO *RADIO_CIRCULO);
-console.log(`El área del círculo es: ${AREA_CIRCULO} unidades cuadradas`);
+function areaCirculo(radio) {
+    return PI * (radio * radio);
+} 
+
 console.groupEnd(); 
